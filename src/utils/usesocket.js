079e7d9a96895ca5fea91925e9bbe22cc09f4b02
2499735779 <ku200426@example.com>
2025-05-20 20:34:58 +0800

    modified:   src/utils/usesocket.js

--- src/utils/usesocket.js
+++ src/utils/usesocket.js
@@ -1,27 +1,25 @@
-// src/composables/useSocket.js
 import { io } from 'socket.io-client';
 import { ref } from 'vue';
 
-// 可以将 socket 实例包装为 ref 以保证响应性，也确保只初始化一次
+// 包装 socket 实例为 ref 保证响应性并确保只初始化一次
 const socket = ref(null);
 
 export function useSocket() {
-  // 单例模式，确保只建立一次连接
+  // 单例模式，确保仅建立一次连接
   if (!socket.value) {
-    // 注意替换 URL 为你的后端 WebSocket 地址
-    socket.value = io("http://117.72.108.239:3001", {
-      // 可以增加额外配置项，如鉴权 token 等
+    // 修改 URL 为 HTTPS 模式，让连接使用 wss:// 协议
+    socket.value = io("https://117.72.108.239", {
       transports: ["websocket"],
     });
     console.log("WebSocket 已初始化");
   }
 
-  // 提供一个加入房间的封装方法
+  // 封装加入房间的方法
   const joinRoom = (username) => {
     socket.value.emit('join', username);
   };
 
-  // 可以封装其他操作，如断开连接
+  // 封装断开连接的方法
   const disconnect = () => {
     if (socket.value) {
       socket.value.disconnect();
